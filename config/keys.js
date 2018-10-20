da7004c9db758102a08d22b7a9f2113b3a3854a1
Rajkumar <rkjmb123@gmail.com>
2018-10-20 18:43:21 +0530

    validation and routes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://rajkumar:rajkumar123@ds237373.mlab.com:37373/miniproject"
+  mongoURI:
+    "mongodb://rajkumar:rajkumar123@ds237373.mlab.com:37373/miniproject",
+    secret: "secret"
 };
